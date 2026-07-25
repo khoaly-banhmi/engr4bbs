bcdd5d1642e1f413bf32c3360c0d17973c045877
Khoa Lý <khoaly@macbookair.mynetworksettings.com>
2026-07-25 14:41:31 -0500

    Quartz sync: Jul 25, 2026, 2:41 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "engr4bbs.khoaly.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
